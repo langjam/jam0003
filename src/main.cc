4a8b429694ba27d81f468454a96949ec0ec431b3
ghostway <50481015+OfekShochat@users.noreply.github.com>
2022-07-23 00:10:43 +0300

    setup good cmake

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 #include <sstream>
 
 auto read_file(std::string file_path) -> std::string {
-  std::ifstream t("file.txt");
+  std::ifstream t(file_path);
   std::stringstream buffer;
   buffer << t.rdbuf();
   return buffer.str();
