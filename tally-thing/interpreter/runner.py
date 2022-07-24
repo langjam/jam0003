2e61d1569f7ccc3031f3b7bada5db3cf50042a52
Daniel Sherlock <d.t.sherlock@live.co.uk>
2022-07-24 19:14:37 +0100

    tally-thing: First full program executed
    Admittedly, it's a no-op, but the program says to do a no-op.
    If it didn't say to do a no-op, it would error,
    because that's all it can do.

--- tally-thing/interpreter/runner.py
+++ tally-thing/interpreter/runner.py
@@ -5,10 +5,14 @@ def run_program(program, json):
   print(program) #for debug
   print(json) #also debug
   
-  for expr in program.exprs:
-    apply_expr(expr, json, {})
+  global lets
+  lets = program.lets
   
-  return "Done!"
+  for expr in program.exprs:
+    json = apply_expr(expr, json, {})
+  
+  print("Done!")
+  return json
 
 def apply_expr(expr, json, env):
   print("-- applying ", expr, " to ", json)
@@ -17,4 +21,15 @@ def apply_expr(expr, json, env):
   ## but I don't usually write python and prefer it this way.
   match expr:
     case Term(name, params):
-      print("It's a term!")
+      print("----TERM")
+      if name == "BUILTIN": return apply_builtin(params[0], json)
+      if name not in lets: raise Exception("Use of non-existent term '" + name + "'")
+      j = json
+      for e in lets[name].exprs:
+        j = apply_expr(e, j, env) # TODO env should be adjusted for lets[name].params
+      return j
+
+def apply_builtin(which, json):
+  print("-- applying BUILTIN ", which, " to ", json)
+  match which:
+    case "identity": return json
