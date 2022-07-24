13d998ff61e9ba4a62e64a8af2feba35e4013edf
Daniel Sherlock <d.t.sherlock@live.co.uk>
2022-07-24 17:56:18 +0100

    tally-thing: Add ability to take in json and pass to runner module

--- tally-thing/interpreter/__main__.py
+++ tally-thing/interpreter/__main__.py
@@ -1,4 +1,4 @@
-# run using `pipenv run python3 . examples\test.tally`
+# run using `pipenv run python3 . examples\test.tally examples\test.json`
 
 import argparse
 
@@ -6,13 +6,16 @@ intake = argparse.ArgumentParser(
   description='Interpreter for the tally-thing DSL'
 )
 intake.add_argument('file', help='the tally script file to be interpreted')
-#intake.add_argument('data', help='the data file to be processed by your script')
+intake.add_argument('data', help='the data file to be processed by your script')
 args = intake.parse_args()
 
-print("You wish to interpret: ", args.file)
-print("Unfortunately, I can't. Yet.")
+import json
 
 from parser import parse_file
+from runner import run_program
 
-print("\nInstead, I'll at least try and parse it:")
-print(parse_file(args.file))
+program = parse_file(args.file)
+with open(args.data, "r") as data_file:
+    data = json.load(data_file)
+    result = run_program(program, data)
+    print(result)
---
+++ tally-thing/interpreter/runner.py
@@ -0,0 +1,6 @@
+# runner.
+
+def run_program(program, json):
+  print(program) #for debug
+  print(json) #also debug
+  return "Done!"
